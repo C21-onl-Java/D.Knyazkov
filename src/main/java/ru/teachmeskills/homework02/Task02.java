67a439880acd732e99cb7630293241cc1d8e852d
knyazkov_d <d.knyazkov@inbox.ru>
2023-04-06 19:58:53 +0300

    test

--- src/main/java/ru/teachmeskills/homework02/Task02.java
+++ src/main/java/ru/teachmeskills/homework02/Task02.java
@@ -1,4 +1,12 @@
 package ru.teachmeskills.homework02;
 
+import java.util.Scanner;
+
 public class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Введите имя:");
+        String name=scanner.nextLine();
+        System.out.printf("Вы ввели %s",name);
+    }
 }
