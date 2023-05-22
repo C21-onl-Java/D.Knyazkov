94515ec2cf89e8cbdcb89cdce02c5034bac82f38
knyazkov_d <d.knyazkov@inbox.ru>
2023-05-22 12:20:52 +0300

    homework09

---
+++ src/main/java/ru/teachmeskills/homework09/Vector3D.java
@@ -0,0 +1,30 @@
+package ru.teachmeskills.homework09;
+
+public class Vector3D extends Vector{
+
+
+    public Vector3D(int size) {
+        super(2);
+    }
+
+    @Override
+    public double calculateScalar() {
+
+        return 0; //возвращаем результат формулы по вычислению скалярного произведения
+    }
+
+    @Override
+    public double calculateSum() {
+        return 0;//возвращаем результат формулы по вычислению суммы векторов
+    }
+
+    @Override
+    public double calculateDifference() {
+        return 0;//возвращаем результат формулы по вычислению разности векторов
+    }
+
+    @Override
+    public double calculateLength() {
+        return 0;//возвращаем результат формулы по вычислению длины векторов
+    }
+}
