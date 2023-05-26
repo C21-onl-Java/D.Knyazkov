e8cd34166ac6c0a28392e051c06a6d621262c2cd
knyazkov_d <d.knyazkov@inbox.ru>
2023-05-26 13:24:28 +0300

    homework11

---
+++ src/main/java/ru/teachmeskills/homework11/task1/Series.java
@@ -0,0 +1,31 @@
+package ru.teachmeskills.homework11.task1;
+
+public class Series {
+    static   String numberOfDocument="2331-QqI-5529-MhG-1u6L";
+
+
+    public void checkNumber(String text) throws WrongContainsException {
+
+        String  newNumberOfDocument=numberOfDocument.toLowerCase();
+        String newText=text.toLowerCase();
+        if (!(newNumberOfDocument.contains(newText)))
+        {throw new WrongContainsException("Слово не содержит нужной последовательности");}
+    }
+
+
+    public  void checkSequencetoStart(String text) throws WrongStartException {
+        if(!(text.startsWith("555")));
+        {throw new WrongStartException("Слово начинается с неверной последовательности");}
+    }
+
+
+
+    public void checkSequencetoFinish(String text) throws WrongFinishException {
+
+        if (!(text.endsWith("1a2b")));
+        {throw new WrongFinishException("Слово заканчивается неверной последовательностью");}
+    }
+
+
+
+}
---
+++ src/main/java/ru/teachmeskills/homework11/task1/WrongFinishException.java
@@ -0,0 +1,10 @@
+package ru.teachmeskills.homework11.task1;
+
+public class WrongFinishException extends Exception{
+
+
+
+    public WrongFinishException(String message) {
+        super(message);
+    }
+}
